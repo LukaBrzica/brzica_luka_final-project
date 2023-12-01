34a383371cc300a91ed9a60b9ca8e72f59e28910
LukaBrzica <Luka.Brzica24@bcp.org>
2023-12-01 09:19:56 -0800

    first push from github desktop

---
+++ main.py
@@ -0,0 +1,11 @@
+# File created by Luka Brzica on 11/26
+
+'''
+Project idea:
+Create a retro soccer game 
+
+Goals:
+create players you can control 
+goals and scoreboard 
+play against computer 
+'''
